c87a060e53834f6567362af81239330d4a166096
Fritz Lekschas <service@lekschas.de>
2017-02-26 17:50:46 -0500

    Update fragment extraction by loci

--- fragments/views.py
+++ fragments/views.py
@@ -2,16 +2,21 @@ from __future__ import print_function
 
 from django.core.cache import cache
 
+from rest_framework.authentication import BasicAuthentication
+from drf_disable_csrf import CsrfExemptSessionAuthentication
+
 import hashlib
+import json
 import logging
 import numpy as np
-from django.views.decorators.csrf import csrf_exempt
 
 from os import path
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes
 
 from tilesets.models import Tileset
+
+from operator import itemgetter
 
 from fragments.utils import (
     calc_measure_dtd,
@@ -28,8 +33,8 @@ logger = logging.getLogger(__name__)
 SUPPORTED_MEASURES = ['distance-to-diagonal', 'noise', 'size', 'sharpness']
 
 
-@csrf_exempt
 @api_view(['POST'])
+@authentication_classes((CsrfExemptSessionAuthentication, BasicAuthentication))
 def fragments_by_loci(request):
     '''
     Retrieve a list of locations and return the corresponding matrix fragments
@@ -43,37 +48,10 @@ def fragments_by_loci(request):
 
     '''
 
-    cooler_file = request.data.get('cooler', False)
-
-    if cooler_file:
-        if cooler_file.endswith('.cool'):
-            cooler_file = path.join('data', cooler_file)
-        else:
-            try:
-                cooler_file = Tileset.objects.get(uuid=cooler_file).datafile
-            except AttributeError:
-                return JsonResponse({
-                    'error': 'Cooler file not in database',
-                }, status=500)
-    else:
-        return JsonResponse({
-            'error': 'Cooler file not specified',
-        }, status=500)
-
     loci = request.data.get('loci', [])
 
     try:
-        zoomout_level = int(request.data.get('zoomoutLevel', -1))
-    except ValueError:
-        zoomout_level = -1
-
-    try:
-        limit = int(request.data.get('limit', -1))
-    except ValueError:
-        limit = -1
-
-    try:
-        precision = int(request.data.get('precision', False))
+        precision = int(request.GET.get('precision', False))
     except ValueError:
         precision = False
 
@@ -82,32 +60,60 @@ def fragments_by_loci(request):
     except ValueError:
         no_cache = False
 
+    '''
+    Loci list must be of type:
+    0: chrom1
+    1: start1
+    2: end1
+    3: chrom2
+    4: start2
+    5: end2
+    6: dataset
+    7: zoomOutLevel [0]
+    '''
+
+    i = 0
+    loci_lists = {}
     try:
-        loci_list = []
         for locus in loci:
-            loci_list.append([
-                locus['chrom1'],
-                locus['start1'],
-                locus['end1'],
-                locus['chrom2'],
-                locus['start2'],
-                locus['end2']
-            ])
+            cooler_file = ''
+
+            if locus[6]:
+                if locus[6].endswith('.cool'):
+                    cooler_file = path.join('data', locus[6])
+                else:
+                    try:
+                        cooler_file = Tileset.objects.get(
+                            uuid=locus[6]
+                        ).datafile
+                    except AttributeError:
+                        return JsonResponse({
+                            'error': 'Dataset (cooler file) not in database',
+                        }, status=500)
+            else:
+                return JsonResponse({
+                    'error': 'Dataset (cooler file) not specified',
+                }, status=500)
+
+            if cooler_file not in loci_lists:
+                loci_lists[cooler_file] = {}
+
+            if locus[7] not in loci_lists[cooler_file]:
+                loci_lists[cooler_file][locus[7]] = []
+
+            loci_lists[cooler_file][locus[7]].append(locus[0:6] + [i])
+
+            i += 1
+
     except Exception as e:
         return JsonResponse({
             'error': 'Could not convert loci.',
             'error_message': str(e)
         }, status=500)
 
-    # Get a unique string for the URL query string
+    # Get a unique string for caching
     uuid = hashlib.md5(
-        '-'.join([
-            cooler_file,
-            loci,
-            str(limit),
-            str(precision),
-            str(zoomout_level)
-        ])
+        json.dumps(loci, sort_keys=True) + str(precision)
     ).hexdigest()
 
     # Check if something is cached
@@ -117,42 +123,45 @@ def fragments_by_loci(request):
         if results:
             return JsonResponse(results)
 
-    if limit > 0:
-        loci_list = loci_list[:limit]
-
+    matrices = []
     try:
-        matrices = get_frag_by_loc(
-            cooler_file,
-            loci_list,
-            zoomout_level=zoomout_level
-        )
+        for dataset in loci_lists:
+            for zoomout_level in loci_lists[dataset]:
+                raw_matrices = get_frag_by_loc(
+                    dataset,
+                    loci_lists[dataset][zoomout_level],
+                    zoomout_level=zoomout_level
+                )
+
+                if precision > 0:
+                    raw_matrices = np.around(raw_matrices, decimals=precision)
+
+                i = 0
+                for raw_matrix in raw_matrices:
+                    matrices.append({
+                        'id': loci_lists[dataset][zoomout_level][i][6],
+                        'raw': raw_matrix
+                    })
+                    i += 1
     except Exception as e:
         return JsonResponse({
             'error': 'Could not retrieve fragments.',
             'error_message': str(e)
         }, status=500)
 
-    if precision > 0:
-        matrices = np.around(matrices, decimals=precision)
+    # Sort matrices
+    matrices_sorted = sorted(matrices, key=itemgetter('id'))
 
     fragments = []
 
     i = 0
-    for matrix in matrices:
-        frag_obj = {
-            'matrix': matrix.tolist()
-        }
-        frag_obj.update(loci[i])
-        fragments.append(frag_obj)
+    for matrix in matrices_sorted:
+        fragments.append(matrix['raw'].tolist())
         i += 1
 
     # Create results
     results = {
-        'count': matrices.shape[0],
-        'dim': matrices.shape[1],
-        'fragments': fragments,
-        'relativeLoci': True,
-        'zoomoutLevel': zoomout_level
+        'fragments': fragments
     }
 
     # Cache results for an hour
@@ -162,6 +171,7 @@ def fragments_by_loci(request):
 
 
 @api_view(['GET'])
+@authentication_classes((CsrfExemptSessionAuthentication, BasicAuthentication))
 def fragments_by_chr(request):
     chrom = request.GET.get('chrom', False)
     cooler_file = request.GET.get('cooler', False)
@@ -354,6 +364,7 @@ def fragments_by_chr(request):
 
 
 @api_view(['GET'])
+@authentication_classes((CsrfExemptSessionAuthentication, BasicAuthentication))
 def loci(request):
     chrom = request.GET.get('chrom', False)
     loop_list = request.GET.get('loop-list', False)
